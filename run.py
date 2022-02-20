0273f0900da1a624424b9655ff03ea3f66b89a67
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-02-20 12:14:13 +0300

    if the user inputs a wrong username or password they recieve an invalid message and are prompted to input correct details .

--- run.py
+++ run.py
@@ -57,7 +57,14 @@ def main():
             print('Enter password')
             default_user_password = input()
             print('/n')
-            
+            while default_user_name != 'testuser' or default_user_password != '0000':
+                print('wrong userName or password. Username 'testuser' and password  '0000'')
+                print('Enter user name')
+                default_user_name
+
+                print('Enter password')
+                default_user_password = input()
+                print('/n')
 
 
         
