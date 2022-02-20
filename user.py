0bdc21a62dd1a4f09f517752bc9727aec0959ad2
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-02-20 10:55:13 +0300

    created run.py.

---
+++ user.py
@@ -0,0 +1,48 @@
+from ssl import _PasswordType
+from typing_extensions import Self
+
+
+class User:
+
+    #class that generates new instance of user
+
+user_list = []
+
+def _init_[self.user_name.password]:
+    self.user_name = user_name
+    self.password = password
+
+def save_user[self]:
+
+    #save_user method save a new user objects to the user_list
+
+    User user_list.append(self)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
