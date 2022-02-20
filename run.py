e0e564fbfaa554695124b8246a77830501dbba37
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-02-20 11:10:13 +0300

    if user says new they are prompted to create new user name

--- run.py
+++ run.py
@@ -2,4 +2,13 @@ import random
 from user import User
 
 
-def main()
+def main():
+
+    while True:
+        print("Welcome to password locker")
+        print('/n')
+        print("Select a short code to navigate through:to create new user 'new':To login to your account 'log' or 'go' to exit")
+        short_code =input().lower()
+        print('/n')
+
+        
